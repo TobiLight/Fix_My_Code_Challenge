1197cb54a773273f6c562d47d2dbada078f3ebc7
TobiLight <jgetitdonefast@gmail.com>
2023-10-08 23:23:51 +0100

    Added docstring to square functions

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -24,6 +24,7 @@ class Square:
         """String representation."""
         return "{}/{}".format(self.width, self.height)
 
+
 if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
