752f364efc2845fef85a5e82393ae33084992888
TobiLight <jgetitdonefast@gmail.com>
2023-10-08 23:14:10 +0100

    Added docstring to square functions

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Square module"""
 
 
 class square:
@@ -7,25 +8,25 @@ class square:
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """ Init sequence. """
+        """ Initialize the square """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def area_of_my_square(self):
+    def area(self):
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def perimeter(self):
         """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ String representation """
+        """ String representation of the square """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
-    print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.area())
+    print(s.perimeter())
