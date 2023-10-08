03ea1a5d9c57bb65ea4a73c2eda66a4413f6243b
TobiLight <jgetitdonefast@gmail.com>
2023-10-08 23:10:40 +0100

    Added docstring to square functions

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-class square():
 
+class square():
     width = 0
     height = 0
 
@@ -24,7 +24,6 @@ class square():
 
 
 if __name__ == "__main__":
-
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
